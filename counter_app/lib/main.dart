4752cea009db8de4f0a13850beb9dea132f56baf
CodingKayak <codingkayak@gmail.com>
2021-06-24 18:07:25 -0300

    dia 24-06-21

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(app);
+  runApp(MaterialApp(
+    title: 'Meu primeiro app',
+    theme: ThemeData(
+      primarySwatch: Colors.green
+    ),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Meu Primeiro App"),
+      ),
+      body: Center(
+        child: Text(
+          "Contador\n0",
+          textAlign: TextAlign.center,
+        ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
+    )
+  ));
 }
